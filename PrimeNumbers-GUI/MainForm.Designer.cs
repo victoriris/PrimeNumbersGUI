559f46ec43e4fbc0624c724c0fc7b8321ac658fc
Victor Iris <vmiris@harding.edu>
2019-09-16 17:19:37 -0500

    Initial Commit

---
+++ PrimeNumbers-GUI/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PrimeNumbers_GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startNumTextBox = new System.Windows.Forms.TextBox();
+            this.endNumTextBox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.numbersTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prime numbers:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Range:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "to";
+            // 
+            // startNumTextBox
+            // 
+            this.startNumTextBox.Location = new System.Drawing.Point(16, 25);
+            this.startNumTextBox.Name = "startNumTextBox";
+            this.startNumTextBox.Size = new System.Drawing.Size(100, 20);
+            this.startNumTextBox.TabIndex = 3;
+            this.startNumTextBox.Text = "1000";
+            // 
+            // endNumTextBox
+            // 
+            this.endNumTextBox.Location = new System.Drawing.Point(16, 64);
+            this.endNumTextBox.Name = "endNumTextBox";
+            this.endNumTextBox.Size = new System.Drawing.Size(100, 20);
+            this.endNumTextBox.TabIndex = 4;
+            this.endNumTextBox.Text = "4000";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 128);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Visible = false;
+            // 
+            // numbersTextBox
+            // 
+            this.numbersTextBox.Location = new System.Drawing.Point(144, 25);
+            this.numbersTextBox.Multiline = true;
+            this.numbersTextBox.Name = "numbersTextBox";
+            this.numbersTextBox.ReadOnly = true;
+            this.numbersTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.numbersTextBox.Size = new System.Drawing.Size(128, 126);
+            this.numbersTextBox.TabIndex = 6;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(16, 168);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 7;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Enabled = false;
+            this.pauseButton.Location = new System.Drawing.Point(106, 168);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 8;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Enabled = false;
+            this.cancelButton.Location = new System.Drawing.Point(197, 168);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 9;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 203);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.numbersTextBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.endNumTextBox);
+            this.Controls.Add(this.startNumTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Prime Numbers";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox startNumTextBox;
+        private System.Windows.Forms.TextBox endNumTextBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox numbersTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
+
---
+++ PrimeNumbers-GUI/MainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers_GUI
+{
+    public partial class MainForm : Form
+    {
+        private CancellationTokenSource cancellationTokenSource;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // Find all prime numbers starting between the first and last numbers
+            int firstNum = Convert.ToInt32(startNumTextBox.Text);
+            int lastNum = Convert.ToInt32(endNumTextBox.Text);
+
+            numbersTextBox.Clear();
+
+            // Prevent user from messing with certain controls while job is running
+            progressBar1.Minimum = firstNum;
+            progressBar1.Maximum = lastNum;
+            progressBar1.Visible = true;
+            cancelButton.Enabled = true;
+            pauseButton.Enabled = true;            
+            startNumTextBox.Enabled = false;
+            endNumTextBox.Enabled = false;
+
+            UseWaitCursor = true;
+
+            // See which numbers are factors and append them to the numbers text box
+            for (int i = firstNum; i <= lastNum; i++)
+            {
+                if (IsPrime(i))
+                {
+                    AddNumberToTextBox(i);
+                }
+            }
+
+            // Let the user know we did something even if no prime nums were found
+            if (numbersTextBox.TextLength == 0)
+            {
+                numbersTextBox.Text = "None.";
+            }
+
+            UseWaitCursor = false;
+
+            // Reset the form
+            startNumTextBox.Enabled = true;
+            endNumTextBox.Enabled = true;
+            progressBar1.Value = progressBar1.Minimum;
+            progressBar1.Visible = false;
+            cancelButton.Enabled = false;
+            pauseButton.Enabled = false;
+        }
+
+        private bool IsPrime(int num)
+        {
+            if (num < 2)
+                return false;
+
+            // Look for a number that evenly divides the num
+            for (int i = 2; i <= num / 2; i++)
+                if (num % i == 0)
+                    return false;
+
+            // No divisors means the number is prime
+            return true;
+        }
+
+        private void AddNumberToTextBox(int num)
+        {
+            numbersTextBox.AppendText(num.ToString());
+            numbersTextBox.AppendText(Environment.NewLine);
+            progressBar1.Value = num;
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            // Pause or resume the current job 
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            // Cancel the work done in the for loop
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
