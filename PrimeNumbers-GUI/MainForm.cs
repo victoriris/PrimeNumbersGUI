d4314d9e3b9693cfc1ece86ffda422d3a02280cf
Victor Iris <vmiris@harding.edu>
2019-09-17 16:38:05 -0500

    Fix project for submit

--- PrimeNumbers-GUI/MainForm.cs
+++ PrimeNumbers-GUI/MainForm.cs
@@ -8,39 +8,49 @@ namespace PrimeNumbers_GUI
     public partial class MainForm : Form
     {
         private CancellationTokenSource cancellationTokenSource;
+        private int lastProgressValue;
+        private int lastNum;
+        private bool has_ended = false;
 
         public MainForm()
         {
             InitializeComponent();
         }
         
-        private void startButton_Click(object sender, EventArgs e)
+        private async void startButton_Click(object sender, EventArgs e)
         {
             // Find all prime numbers starting between the first and last numbers
-            int firstNum = Convert.ToInt32(startNumTextBox.Text);
-            int lastNum = Convert.ToInt32(endNumTextBox.Text);
+            int firstNum;
+
+            try
+            {
+                firstNum = Convert.ToInt32(startNumTextBox.Text);
+                lastNum = Convert.ToInt32(endNumTextBox.Text);
+            } catch (FormatException)
+            {
+                MessageBox.Show("Invalid Input", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
             numbersTextBox.Clear();
-
+            
             // Prevent user from messing with certain controls while job is running
             progressBar1.Minimum = firstNum;
             progressBar1.Maximum = lastNum;
             progressBar1.Visible = true;
             cancelButton.Enabled = true;
-            pauseButton.Enabled = true;            
+            pauseButton.Enabled = true;
             startNumTextBox.Enabled = false;
+            startButton.Enabled = false;
             endNumTextBox.Enabled = false;
+
 
             UseWaitCursor = true;
 
-            // See which numbers are factors and append them to the numbers text box
-            for (int i = firstNum; i <= lastNum; i++)
-            {
-                if (IsPrime(i))
-                {
-                    AddNumberToTextBox(i);
-                }
-            }
+            // Task
+            Task getPrimeTask = getPrimes(firstNum, lastNum);
+            await getPrimeTask;
+
 
             // Let the user know we did something even if no prime nums were found
             if (numbersTextBox.TextLength == 0)
@@ -48,15 +58,58 @@ namespace PrimeNumbers_GUI
                 numbersTextBox.Text = "None.";
             }
 
+
             UseWaitCursor = false;
 
+            if (has_ended)
+            {
+                resetForm();
+            }
+
+            
+        }
+
+        private void resetForm ()
+        {
             // Reset the form
+            has_ended = false;
+            pauseButton.Text = "Pause";
             startNumTextBox.Enabled = true;
             endNumTextBox.Enabled = true;
             progressBar1.Value = progressBar1.Minimum;
             progressBar1.Visible = false;
             cancelButton.Enabled = false;
+            startButton.Enabled = true;
             pauseButton.Enabled = false;
+        }
+
+        private async Task getPrimes(int firstNum, int lastNum)
+        {
+
+            cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+
+            await Task.Run(() => {
+                for (int i = firstNum; i <= lastNum; i++)
+                {
+                    lastProgressValue = i;
+                    if (token.IsCancellationRequested)
+                    {
+                        break;
+                    }
+
+                    // See which numbers are factors and append them to the numbers text box
+                    if (IsPrime(i))
+                    {
+                        AddNumberToTextBox(i);
+                    }
+                }
+                if (lastProgressValue == lastNum)
+                {
+                    has_ended = true;
+                }
+            });
+
         }
 
         private bool IsPrime(int num)
@@ -75,24 +128,48 @@ namespace PrimeNumbers_GUI
 
         private void AddNumberToTextBox(int num)
         {
-            numbersTextBox.AppendText(num.ToString());
-            numbersTextBox.AppendText(Environment.NewLine);
-            progressBar1.Value = num;
+            Invoke((Action)delegate ()
+           {
+               numbersTextBox.AppendText(num.ToString());
+               numbersTextBox.AppendText(Environment.NewLine);
+               progressBar1.Value = num;
+           });
+            
         }
 
-        private void pauseButton_Click(object sender, EventArgs e)
+        private async void pauseButton_Click(object sender, EventArgs e)
         {
-            // Pause or resume the current job 
+            if (pauseButton.Text == "Pause")
+            {
+                // Pause the current job 
+                cancellationTokenSource.Cancel();
+                pauseButton.Text = "Resume";
+            }
+            else
+            {
+                // Resume the current job 
+                pauseButton.Text = "Pause";
+                await getPrimes(lastProgressValue, lastNum);
+                if (lastProgressValue == lastNum)
+                {
+                    resetForm();
+                }
+            }
+           
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
         {
             // Cancel the work done in the for loop
+            has_ended = true;
+            cancellationTokenSource.Cancel();
+            resetForm();
+            pauseButton.Enabled = false;
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
